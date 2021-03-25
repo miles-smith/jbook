ee509e720131049edb581f6fc2c2b77b7e79260c
Miles Smith <miles-smith@users.noreply.github.com>
2021-03-25 10:51:01 +0000

    Quick and dirty proof of concept for transpiling code within the browser

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,7 +1,44 @@
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return(<h1>Hello World</h1>);
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const service = useRef<any>();
+
+  const startService = async () => {
+    service.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  const onClick = async () => {
+    if(!service.current)
+      return;
+
+    const result = await service.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
+  }
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  return(
+    <div>
+      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 }
 
 ReactDOM.render(
