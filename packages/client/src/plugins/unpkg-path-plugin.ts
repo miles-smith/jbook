e25cbed28a81506b98dacb02efef25a5ec27ae2a
Miles Smith <miles-smith@users.noreply.github.com>
2021-04-19 07:16:25 +0100

    Add caching layer to avoid constantly hitting unpkg.com

--- packages/client/src/plugins/unpkg-path-plugin.ts
+++ packages/client/src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache',
+});
 
 export const unpkgPathPlugin = () => {
   const rootUrl = 'https://unpkg.com';
@@ -46,13 +51,22 @@ export const unpkgPathPlugin = () => {
           };
         }
 
-        const { data, request } = await axios.get(args.path);
+        const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-        return {
+        if (cacheResult) {
+          return cacheResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
