b87a15955a0b66b078010c8f9463120581cf8c07
Miles Smith <miles-smith@users.noreply.github.com>
2021-04-19 10:09:32 +0100

    Break up package loading with multiple onLoad functions, based on type.

--- packages/client/src/plugins/fetch-plugin.ts
+++ packages/client/src/plugins/fetch-plugin.ts
@@ -10,14 +10,14 @@ export const fetchPlugin = (userCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: userCode,
-          };
-        }
+      build.onLoad({ filter: /^index\.js$/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: userCode,
+        };
+      });
 
+      build.onLoad({ filter: /\.css$/ }, async (args: any) => {
         const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
         if (cacheResult) {
@@ -26,23 +26,36 @@ export const fetchPlugin = (userCode: string) => {
 
         const { data, request } = await axios.get(args.path);
 
-        const fileType = args.path.match(/\.css$/) ? 'css' : 'jsx';
-        const contents =
-          fileType === 'css'
-            ?
-              `
-              const style = document.createElement('style');
-              style.setAttribute('type', 'text/css');
-              style.innerText = '@import url("data:text/css;base64,${btoa(data)}")';
-              document.head.appendChild(style);
-              `
-            :
-              data;
-
+        const contents = `
+          const style = document.createElement('style');
+          style.setAttribute('type', 'text/css');
+          style.innerText = '@import url("data:text/css;base64,${btoa(data)}")';
+          document.head.appendChild(style);
+        `;
 
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(args.path, result);
+
+        return result
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        if (cacheResult) {
+          return cacheResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
 
