a1689739d3719c771baac13a854f17fe652a563c
Miles Smith <miles-smith@users.noreply.github.com>
2021-05-10 17:58:58 +0100

    Package editor and preview into a resuable component.

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,27 +1,12 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import CodeEditor from './components/code-editor';
-import CodePreview from './components/code-preview';
-import bundle from './bundler';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode]   = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  }
-
   return(
     <div>
-      <CodeEditor initialValue="" onChange={value => setInput(value)} />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <CodePreview code={code} />
+      <CodeCell />
     </div>
   );
 }
