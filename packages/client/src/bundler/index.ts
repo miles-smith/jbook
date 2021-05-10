c23e01c7b6f82c3b087b0c0d0b3e2db49e20e58d
Miles Smith <miles-smith@users.noreply.github.com>
2021-05-10 17:45:18 +0100

    Clean up; Extract out bundling logic from App component.

---
+++ packages/client/src/bundler/index.ts
@@ -0,0 +1,30 @@
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
+import { fetchPlugin } from '../plugins/fetch-plugin';
+
+let service: esbuild.Service;
+
+export default async (userCode: string) => {
+  if(!service) {
+    service = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  }
+
+  const bundledCode = await service.build({
+    entryPoints: ['index.js'],
+    bundle: true,
+    write: false,
+    plugins: [
+      unpkgPathPlugin(),
+      fetchPlugin(userCode)
+    ],
+    define: {
+      global: 'window',
+      'process.env.NODE_ENV': '"production"',
+    },
+  });
+
+  return bundledCode.outputFiles[0].text;
+}
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,48 +1,19 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
+
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
 import CodePreview from './components/code-preview';
+import bundle from './bundler';
 
 const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode]   = useState('');
-  const service = useRef<any>();
-
-  const startService = async () => {
-    service.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  };
 
   const onClick = async () => {
-    if(!service.current)
-      return;
-
-    const result = await service.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [
-        unpkgPathPlugin(),
-        fetchPlugin(input)
-      ],
-      define: {
-        global: 'window',
-        'process.env.NODE_ENV': '"production"',
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   }
-
-  useEffect(() => {
-    startService();
-  }, []);
 
   return(
     <div>
