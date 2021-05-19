ec9c301c7f65f38b18f4107f78e4da04c303805f
Miles Smith <miles-smith@users.noreply.github.com>
2021-05-19 14:10:27 +0100

    Debounce resize handler (performance)

--- packages/client/src/components/resizable.tsx
+++ packages/client/src/components/resizable.tsx
@@ -12,9 +12,16 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
 
   useEffect(() => {
+    let timer: any;
     const onResize = () => {
-      setInnerHeight(window.innerHeight);
-      setInnerWidth(window.innerWidth);
+      if(timer) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+      }, 100)
     }
 
     window.addEventListener('resize', onResize);
