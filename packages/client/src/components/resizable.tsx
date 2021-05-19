5a09f5ecc6abf95ec42af0e684294ebc94e84da2
Miles Smith <miles-smith@users.noreply.github.com>
2021-05-19 14:04:44 +0100

    Ensure editor/preview windows are not lost on browser resize

--- packages/client/src/components/resizable.tsx
+++ packages/client/src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import './resizable.css';
+import { useState, useEffect } from 'react';
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
@@ -6,24 +7,39 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth]   = useState(window.innerWidth);
   let resizableProps: ResizableBoxProps;
+
+  useEffect(() => {
+    const onResize = () => {
+      setInnerHeight(window.innerHeight);
+      setInnerWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', onResize);
+
+    return () => {
+      window.removeEventListener('resize', onResize);
+    }
+  }, []);
 
   if(direction === 'vertical') {
     resizableProps = {
       height: 300,
       width: Infinity,
       resizeHandles: ['s'],
-      minConstraints: [Infinity, window.innerHeight * 0.1],
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      minConstraints: [Infinity, innerHeight * 0.1],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       className: 'resize-vertical',
     }
   } else {
     resizableProps = {
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width: innerWidth * 0.75,
       resizeHandles: ['e'],
-      minConstraints: [window.innerWidth * 0.2, Infinity],
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
       className: 'resize-horizontal',
     }
   }
