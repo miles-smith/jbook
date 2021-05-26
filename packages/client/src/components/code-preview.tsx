022f64987bae0dc6f243664488926fac526379b6
Miles Smith <miles-smith@users.noreply.github.com>
2021-05-26 07:51:41 +0100

    Better handle async errors in user code.

--- packages/client/src/components/code-preview.tsx
+++ packages/client/src/components/code-preview.tsx
@@ -13,14 +13,23 @@ const srcHtml = `
       <title></title>
       <style>html { background-color: white; }</style>
       <script>
+        const handleError = (err) => {
+          const root = document.getElementById('root');
+          root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+
+          console.error(err);
+        }
+
+        window.addEventListener('error', (event) => {
+          event.preventDefault();
+          handleError(event.error)
+        });
+
         window.addEventListener('message', (event) => {
           try {
             eval(event.data);
           } catch (err) {
-            const root = document.getElementById('root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-
-            console.error(err);
+            handleError(err);
           }
         }, false);
       </script>
