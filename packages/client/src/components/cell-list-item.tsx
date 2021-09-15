3d8f614952ebb9464d31a276eb6a8fa22581c3d9
Miles Smith <miles-smith@users.noreply.github.com>
2021-09-15 10:58:19 +0100

    Plug ActionBar into CellList

--- packages/client/src/components/cell-list-item.tsx
+++ packages/client/src/components/cell-list-item.tsx
@@ -1,15 +1,41 @@
+import './cell-list-item.css';
+
 import { Cell }  from '../state';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
+import ActionBar from './action-bar';
 
 interface CellListItemProps {
   cell: Cell
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  let child: JSX.Element;
+
+  switch(cell.type) {
+    case 'code':
+      child =
+        <>
+          <div className="action-bar-wrapper">
+            <ActionBar id={cell.id} />
+          </div>
+          <CodeCell cell={cell} />
+        </>;
+      break;
+    case 'text':
+      child =
+        <>
+          <TextEditor cell={cell} />
+          <ActionBar id={cell.id} />
+        </>;
+      break;
+    default:
+      child = <></>;
+  }
+
   return(
-    <div>
-      { cell.type === 'code' ? <CodeCell cell={cell} /> : <TextEditor cell={cell} /> }
+    <div className="cell-list-item">
+      {child}
     </div>
   )
 }
