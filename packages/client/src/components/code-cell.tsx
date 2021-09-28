b9b0dc51f47bf1af9e21c59c855bad3364561d18
Miles Smith <miles-smith@users.noreply.github.com>
2021-09-28 15:43:36 +0100

    Add cumulative code execution.
    This allows code cells to reference code/data from those before it.

--- packages/client/src/components/code-cell.tsx
+++ packages/client/src/components/code-cell.tsx
@@ -15,20 +15,39 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useSelector((state) => {
+    const { data, order } = state.cells;
+
+    const cells = order.map(id => data[id]);
+    const codeBlocks = [];
+
+    for(let c of cells) {
+      if(c.type === 'code') {
+        codeBlocks.push(c.data);
+      }
+
+      if(c.id === cell.id) {
+        break;
+      }
+    }
+
+    return codeBlocks;
+  });
+  console.log(cumulativeCode);
 
   useEffect(() => {
     if(!bundle) {
-      createBundle(cell.id, cell.data);
+      createBundle(cell.id, cumulativeCode.join('\n'));
       return
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.data);
+      createBundle(cell.id, cumulativeCode.join('\n'));
     }, 1000);
 
     return () => { clearTimeout(timer); }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.id, cell.data, createBundle]);
+  }, [cell.id, cumulativeCode.join('\n'), createBundle]);
 
   return(
     <Resizable direction='vertical'>
@@ -36,15 +55,19 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
         <Resizable direction='horizontal'>
           <CodeEditor initialValue={cell.data} onChange={value => updateCell(cell.id, value)} />
         </Resizable>
+        <div className="progress-wrapper">
         {
           !bundle || bundle.loading
-          ? <div className="progress-cover">
-            <progress className="progress is-small is-primary" max="100">
-              Loading...
-            </progress>
-          </div>
-          : <CodePreview code={bundle.code} error={bundle.error} />
+          ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading...
+              </progress>
+            </div>
+          )
+          : (<CodePreview code={bundle.code} error={bundle.error} />)
         }
+        </div>
       </div>
     </Resizable>
   );
